3fa21a3a7b9ed1d225a8693aade8b22a0f94a42a
AmirAmArA <amiramara11@gmail.com>
2019-04-20 19:48:04 +0300

    adding the new enviornment setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import headerComponent from './component';
+
+ReactDOM.render(
+    <headerComponent />,
+    document.querySelector('#container')
+);
---
+++ src/component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+export default class headerComponent extends React.Component {
+    render() {
+        return<div>
+                    <h1>Main Page</h1>
+                 </div>
+    }
+}
