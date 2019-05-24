5eb0e1dd2b19b921fde5ba228f4e797930600563
majdedr <36421816+majdedr@users.noreply.github.com>
2019-05-24 15:16:59 +0300

    updating changes

--- src/App.js
+++ src/App.js
@@ -19,110 +19,49 @@ import EditEvent from "./EditEvent";
 import EventCardToView from "./EventCardToView";
 
 //the context we R going to use
-export const MyContext = React.createContext();
-
+import WishContext from './WishContext';
 //then we also need a provider to the context
-
-export class MyProvider extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loginFlag: true,
-      userid: 0,
-      username: "Majde",
-      age: 10,
-      events: [
-        {
-          id: 1,
-          name: "Mustafa",
-          avatar: "images/avatar.jpg",
-          startdate: "2019-01-01",
-          enddate: "2019-01-01",
-          description: "Marhaba, I love Avatars...",
-          lastUpdateTime: new Date().toLocaleString("he-IL"),
-          callercomp: "ShowUserWishes"
-        },
-        {
-          id: "2",
-          name: "Suhir",
-          avatar: "images/avatar.jpg",
-          startdate: "2019-01-01",
-          enddate: "2019-01-01",
-          description: "Hello, I love Avatars...",
-          lastUpdateTime: new Date().toLocaleString("he-IL"),
-          callercomp: "ShowUserWishes"
-        },
-        {
-          id: 3,
-          name: "Shahar",
-          avatar: "images/avatar2.png",
-          startdate: "2019-01-01",
-          enddate: "2019-01-01",
-          description: "Shalom, I love Avatars...",
-          lastUpdateTime: new Date().toLocaleString("he-IL"),
-          callercomp: "ShowUserWishes"
-        }
-      ]
-
-    };//state
-    this.addADate= this.addADate.bind(this)
-  } //constructor
-
-
-addADate(obj){
-  if(obj !=null){
-
-      // if(obj.eventid>0 &&   ){}//if
-    this.setState({
-      events : [...events , obj]
-    })
-
-
-  }//if
-
-}//addadate()
-
-  render() {
-    return (
-      <MyContext.Provider
-        value={{
-          state: this.state,
-          growAYearOlder: () =>
-            this.setState({
-              age: this.state.age + 1 
-            }) ,
-            addADate : this.addADate 
-            
-        }}
-      >
-        {this.props.children}
-      </MyContext.Provider>
-    );
-  }
-} //class myprovider
 
 export class App extends React.Component {
   constructor() {
     super();
+
+    this.logout = this.logout.bind(this);
+    this.login = this.login.bind(this);
+
+
+    this.state = {
+      userId: -1,
+      username: '',
+      login: this.login,
+    logout: this.logout
+
+    };
+  }
+
+  login(username, password) {
+    this.setState({
+      username
+    });
+  }
+
+  logout(){
+
+    this.setState({
+      username:''
+    });
+
+    // return <Redirect to="/"/>
+    this.props.history.push('/');
+
   }
 
   render() {
     return (
-      <MyProvider>
+      <WishContext.Provider value={this.state}>
         <Router>
           <NavComponent />
-          <div>
-            <MyContext.Consumer>
-              {context => (
-                <React.Fragment>
-                  {/* here we should return one elemenet which i refer to as react fragment */}
-
-                  {/* <p> im inside the consumer {context.state.username}{context.state.age}</p> */}
-                  {/* <button onClick={context.growAYearOlder}> + ++++</button> */}
-                </React.Fragment>
-              )}
-            </MyContext.Consumer>
-          </div>
+          
           {/* <Route 
           path='/'
           render={ props => <MyComponent {...props} />}
@@ -148,7 +87,7 @@ export class App extends React.Component {
             <Route path="/EventCardToView" component={EventCardToView} />
           </Switch>
         </Router>
-      </MyProvider>
+      </WishContext.Provider>
     );
   }
 }
--- src/NavComponent.js
+++ src/NavComponent.js
@@ -1,215 +1,66 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
-import { NavLink } from "react-router-dom";
 
-import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, Nav, Form, Button } from "react-bootstrap";
+
 import "./general.css";
 import logo from "../assets/logo.PNG";
 
-import { App, MyContext, MyProvider } from "./App";
 import ShowUserWishes from "./ShowUserWishes";
 
-export class NavComponent extends React.Component {
-  // Loginbtn  Registerbtn  hellousernameid
-  //  loginbtn= document.getElementById("#Loginbtn");
-  //  registerbtn= document.querySelector("#Registerbtn");
-  //  hellousernameid= document.querySelector("#hellousernameid");
+import WishContext from "./WishContext";
 
+export class NavComponent extends React.Component {
   constructor() {
     super();
-    localStorage.clear();
-    this.state = {
-      loginFlag: true,
-      username: "Majde",
-      userid: 22,
-      counter: 0 //if counter ==1 then dont update the state => in order to render just one time after updating the loginflag
-    };
-    this.checkIfLoggedIn = this.checkIfLoggedIn.bind(this);
-  } //constructor
+  }
 
-  checkIfLoggedIn(flag, username) {
-    if (flag == true) {
-      return (
-        <>
-          <form className="form-inline my-2 my-lg-0">
-            {/* <Link to="/Login"> */}
-
-            <div className={this.state.divhellousername} id="hellousernameid">
-              <span className="colorwhite ">Hi:{username}</span>
-            </div>
-            {/* </Link> */}
-          </form>
-        </>
-      );
-    } //if
-    if (flag == false) {
-      return (
-        <form className="form-inline my-2 my-lg-0">
-          {/* <Link to="/Login"> */}
-          <button
-            id="Loginbtn"
-            className="btn btn-outline-success my-2 my-sm-0"
-          >
-            <NavLink className="nav-link" to="/Login">
-              SignIn
-            </NavLink>
-          </button>
-          <button
-            id="Registerbtn"
-            className="btn btn-outline-success my-2 my-sm-0"
-          >
-            <NavLink className="nav-link" to="/Register">
-              Register
-            </NavLink>
-          </button>
-
-        </form>
-      );
-    } //if
-  } //checkIfLoggedIn()
-
-  //inside render: if loggedin => run function that return the label , else run func. that retu. 2 buttons
   render() {
-    const style = this.state.loginFlag ? { display: "none" } : {};
-    const style1 = this.state.loginFlag ? {} : { display: "none" };
-    
-    let myeventslink = "/MyEvents/" + this.state.userid;
-    let ShowUserWishes = "/ShowUserWishes/" + this.state.userid;
-    // const isLoggedIn = localStorage.getItem("loginflag");
-    if (this.state.loginFlag) {
-      console.log("user is logged in");
-      return (
-        // <Router>
-        <MyContext.Consumer>
-          {context => (
-            <nav className="navbar navbar-expand-lg navbar-dark navStyle">
-              <a className="navbar-brand">
-                <img src={logo} className="logo" />
-              </a>
+    const { username, logout } = this.context;
 
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon" />
-              </button>
-
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item ">
-                    <NavLink className="nav-link" exact to="/">
-                      Home
-                    </NavLink>
-
-                    {/* <a className="nav-link" href="/"></a> */}
-                  </li>
-                  <li className="nav-item">
-                    {/* /MyEvents */}
-                    <NavLink className="nav-link" to={myeventslink}>
-                      MyEvents
-                    </NavLink>
-
-                    {/* <a className="nav-link" href="/MyEvents"></a> */}
-                  </li>
-                  <li className="nav-item ">
-                    {/* /ShowUserWishes */}
-                    <NavLink className="nav-link" to={ShowUserWishes}>
-                      MyWishes
-                    </NavLink>
-
-                    {/* <a className="nav-link" href="/ShowUserWishes"></a> */}
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/about">
-                      About
-                    </NavLink>
-                  </li>
-                </ul>
-                {this.checkIfLoggedIn(
-                  context.state.loginFlag,
-                  context.state.username
-                )}
-              </div>
-            </nav>
-          )}
-        </MyContext.Consumer>
-        // </Router>
-      );
-    } else {
-      console.log("user is not logged in");
-      return (
-        // <Router>
-        <MyContext.Consumer>
-          {context => (
-            <nav className="navbar navbar-expand-lg navbar-dark navStyle">
-              <a className="navbar-brand">
-                <img src={logo} className="logo" />
-              </a>
-
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon" />
-              </button>
-
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item ">
-                    <NavLink className="nav-link" to="/">
-                      Home
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/Register">
-                      MyEvents
-                    </NavLink>
-                  </li>
-                  <li className="nav-item ">
-                    <NavLink className="nav-link" to="/Register">
-                      MyWishes
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/about">
-                      About
-                    </NavLink>
-                  </li>
-                </ul>
-                {this.checkIfLoggedIn(
-                  context.state.loginFlag,
-                  context.state.username
-                )}
-              </div>
-            </nav>
-          )}
-        </MyContext.Consumer>
-
-        // </Router>
-      );
-    } //else
-    // this.checkIfLogedIn(isLoggedIn);
-  } //render
-} //class
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Best Wishes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Form inline>
+            {username ? (
+              <>
+                Hi {username}!
+                <a
+                  aria-current="page"
+                  className="nav-link active"
+                  href=""
+                  onClick={() => logout()}
+                >
+                  Logout
+                </a>
+              </>
+            ) : (
+              <>
+                <NavLink className="nav-link" to="/Login">
+                  Sign In
+                </NavLink>
+                <NavLink className="nav-link" to="/Register">
+                  Register
+                </NavLink>
+              </>
+            )}
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
 
 // NavComponent.propTypes = {
 //   username: PropTypes.string,
 //   loginFlag: PropTypes.boolean
 //  }
+
+NavComponent.contextType = WishContext;
